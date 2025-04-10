d44e2ae86e01f05edbc7cb2bd415debca8ebd5c8
JoseG1297 <mordo1309@gmail.com>
2025-04-10 16:58:44 -0600

    primeros serrvicios products

---
+++ backend/src/products/products.controller.ts
@@ -0,0 +1,35 @@
+import { Controller, Get, Post, Body, Param, Put, Delete, Req } from '@nestjs/common';
+import { ProductsService } from './products.service';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { Request } from 'express';
+
+@Controller('products')
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
+  @Post()
+  create(@Body() createProductDto: CreateProductDto, @Req() req: Request) {
+    return this.productsService.create(createProductDto, req.user.id);
+  }
+
+  @Get()
+  findAll() {
+    return this.productsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.productsService.findOne(id);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto, @Req() req: Request) {
+    return this.productsService.update(id, updateProductDto, req.user.id);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.productsService.remove(id);
+  }
+}
---
+++ backend/src/products/products.service.ts
@@ -0,0 +1,61 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Product } from './entities/product.entity';
+import { User } from '../users/entities/user.entity';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+
+@Injectable()
+export class ProductsService {
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>
+  ) {}
+
+  async create(createProductDto: CreateProductDto, userId: string) {
+    const user = await this.userRepository.findOne({ where: { id: userId } });
+    if (!user) throw new Error('User not found');
+
+    const product = this.productRepository.create({
+      ...createProductDto,
+      createdBy: user
+    });
+
+    return await this.productRepository.save(product);
+  }
+
+  async findAll() {
+    return await this.productRepository.find({ relations: ['createdBy', 'updatedBy'] });
+  }
+
+  async findOne(id: string) {
+    return await this.productRepository.findOne({
+      where: { id },
+      relations: ['createdBy', 'updatedBy']
+    });
+  }
+
+  async update(id: string, updateProductDto: UpdateProductDto, userId: string) {
+    const user = await this.userRepository.findOne({ where: { id: userId } });
+    if (!user) throw new Error('User not found');
+
+    const product = await this.productRepository.preload({
+      id,
+      ...updateProductDto,
+      updatedBy: user
+    });
+
+    if (!product) throw new Error('Product not found');
+
+    return await this.productRepository.save(product);
+  }
+
+  async remove(id: string) {
+    const product = await this.findOne(id);
+    if (!product) throw new Error('Product not found');
+    return await this.productRepository.remove(product);
+  }
+}
